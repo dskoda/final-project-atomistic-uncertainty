61de740c8c08f079d7ad3f4c5cfb7f45c9d453ac
Franklin Morgan <efranklinmorgan@gmail.com>
2021-04-26 23:14:02 -0400

    Updated intro, svg size, and scroll locations

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -1,14 +1,17 @@
 scenes = [
-    scene1,
+    sceneBlank, 
+    sceneIntro1,
+    sceneIntro1,
+    sceneIntro2,
     scene2,
     sceneGen1,
     sceneGen2,
     sceneGen3,
     sceneGen4,
-    sceneGen5,
+    sceneGen5, 
     sceneGen6,
     sceneGen7,
-    sceneEnd,
+    sceneEnd
 ];
 
 
---
+++ scripts/sections/intro.js
@@ -0,0 +1,11 @@
+const sceneIntro1 = new SvgFileScene(
+    d3.select("#svgIntro1"),
+    contourArea,
+    "figs/intro/pes.svg"
+);
+
+const sceneIntro2 = new SvgFileScene(
+    d3.select("#svgIntro2"),
+    contourArea,
+    "figs/intro/atomsim1.svg"
+);
